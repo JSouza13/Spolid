e2d2c6f97c19fcb0adbfe18540c68698bd9bb8cf
Joao V. de Souza <jooao_souza@hotmail.com>
2020-03-24 20:49:16 -0300

    Cadastro de usuário e autenticação através de email e senha

---
+++ Spolid/src/config/auth.js
@@ -0,0 +1,4 @@
+export default {
+  secret: '5f57834407af5083df3600e46c1ba5c9',
+  expiresIn: '7d',
+};
--- Spolid/src/routes.js
+++ Spolid/src/routes.js
@@ -1,7 +1,11 @@
 import { Router } from 'express';
 
+import UserController from './app/controllers/UserController';
+import SessionController from './app/controllers/SessionController';
+
 const routes = new Router();
 
-routes.get('/', (req, res) => res.json({ message: 'Hello SPOLID hi' }));
+routes.post('/users', UserController.store);
+routes.post('/Sessions', SessionController.store);
 
 export default routes;
